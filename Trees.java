6bc938957aa657f0c1b2d1c0acf8b866a1c04548
masonwaters36 <58090228+masonwaters36@users.noreply.github.com>
2019-11-27 13:37:50 -0600

    Add files via upload

--- Trees.java
+++ Trees.java
@@ -1,7 +1,5 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -16,38 +14,49 @@ public class Trees {
 
     public static void main(String[] args) throws FileNotFoundException {
         Scanner console = new Scanner(System.in);
-        File file = new File("Zippie.txt");
+        File file = new File("zips.txt");
         Scanner data = new Scanner(file);
+        Scanner test = new Scanner(file);
         SplayTree<Place> s = new SplayTree<Place>();
         BinarySearchTree<Place> b = new BinarySearchTree<Place>();
         AVLTree<Place> a = new AVLTree<Place>();
-        addToTrees(data, s,b,a);
+        addToTrees(data, s, b, a);
+        //getResults(test, s, b, a);
         askUser(console, s, b, a);
-
-
     }
 
-
-
-    public static void askUser(Scanner console,SplayTree<Place> s ,BinarySearchTree<Place> b, AVLTree<Place> a) throws FileNotFoundException {
+    /**
+     * This method prints to the user asking if they want to search a tree for a city and state
+     * @param console calls the scanner to the console
+     * @param s is the Splay Tree called
+     * @param b is the BST called
+     * @param a is the AVL tree called
+     */
+    public static void askUser(Scanner console, SplayTree<Place> s, BinarySearchTree<Place> b, AVLTree<Place> a) {
         boolean game = true;
         while (game == true) {
-            s.printLevelOrder();
             System.out.print("You want to search for the city: ");
             String response = console.nextLine();
             Place searchFor = new Place(response, 0000);
-            SplayBSTNode<Place> found = s.search(searchFor);
+            SplayNode<Place> found = s.search(searchFor);
             BSTNode<Place> found1 = b.search(searchFor);
             AVLNode<Place> found2 = a.search(searchFor);
+            if (found1 == null) {
+                System.out.println("There is not a city by this name.");
+            }
             ArrayList<Integer> zipps = found.getElement().getZipCodes();
             System.out.print("The number of comparisons needed to find the entry in BST: ");
-            System.out.println(b.getCompareNum());
+            System.out.println(b.getSearchCounter());
             System.out.print("The number of comparisons needed to find the entry in AVL: ");
-            System.out.println(a.getCompareNum());
+            System.out.println(a.getSearchCounter());
             System.out.print("The number of comparisons needed to find the entry in Splay: ");
-            System.out.println(s.getCompareNum());
+            System.out.println(s.getSearchCounter());
             System.out.print("The zip codes that belong to " + response + " are: ");
-            System.out.println(zipps.toString());
+            if (zipps == null) {
+                //Nothing
+            } else {
+                System.out.println(zipps);
+            }
             System.out.print("Do you want me to search again? ");
             String response2 = console.nextLine();
             if (response2.equalsIgnoreCase("yes")) {
@@ -59,92 +68,99 @@ public class Trees {
         }
     }
 
-    public static void addToTrees(Scanner data, SplayTree<Place> s,BinarySearchTree<Place> b,AVLTree<Place> a) throws FileNotFoundException {
-        //FileOutputStream fout = new FileOutputStream("temporary.txt");            //temp delete later
-        //PrintStream out = new PrintStream(fout);                                        //temp delete later
-/*
-        SplayTree<Place> s = new SplayTree<Place>();
-        BinarySearchTree<Place> b = new BinarySearchTree<Place>();
-        AVLTree<Place> a = new AVLTree<Place>();
-*/
-
-
+    /**
+     * This method adds all the cities and states and their associated zip codes to the trees
+     * @param data is the scanner called
+     * @param s is the Splay Tree called
+     * @param b is the BST called
+     * @param a is the AVL tree called
+     */
+    public static void addToTrees(Scanner data, SplayTree<Place> s, BinarySearchTree<Place> b, AVLTree<Place> a) {
         String firstLine = data.nextLine();//gets ignored
-        //grabs first city data
-        /*
-        int zipCode = data.nextInt();
-        double xValue = data.nextDouble(); //ignore
-        double yValue = data.nextDouble(); //ignore
-        String cityState = data.nextLine().trim();
-
-
-        Place newPlace = new Place(cityState, zipCode);//creates a place for this city data
-        //inserts this place into the tree
-        s.insert(newPlace);
-        b.insert(newPlace);
-        a.insert(newPlace);
-        lastName = cityState.trim();
-
-         */
         String prevName = " ";//used for comparisons between lines
         Place holdPlace = new Place("Hold", 5); //dummy temp place that gets overwritten
         while (data.hasNextLine()) {
             //get the data for a city
             int zipCode1 = data.nextInt();
-            double  xValue1= data.nextDouble(); //ignore
+            double xValue1 = data.nextDouble(); //ignore
             double yValue1 = data.nextDouble(); //ignore
             String cityState1 = data.nextLine().trim();
-
-
-           // Place holdPlace = new Place("Hold", 5);
-            //System.out.println(zipCode1 + " " + xValue1 + " " + yValue1 + " " +cityState1);
-            if(prevName.equalsIgnoreCase(" ")){//the first town to insert
-                Place newPlace2 = new Place(cityState1,zipCode1);//creates a place for first line
+            if (prevName.equalsIgnoreCase(" ")) {//the first town to insert
+                Place newPlace2 = new Place(cityState1, zipCode1);//creates a place for first line
                 s.insert(newPlace2);
                 b.insert(newPlace2);
                 a.insert(newPlace2);
                 holdPlace = newPlace2;//sets the temp place = the first line
-            }
-            else {
+            } else {
                 if (cityState1.equals(prevName)) {
-                    //adds the zip to the place that is in hold because we know that the line we are currently
-                    //on has the same city state as the previous line.
-                    //do not need to do hold = newPlace2 since we never created a new place
-                    System.out.println("Here");
                     holdPlace.addZip(zipCode1);//add zip code to existing city
-                    //System.out.println("HERE2");
-                /*
-                if (!data.hasNextLine()) {                  // Last line of file check
-                    s.insert(newPlace);
-                    b.insert(newPlace);
-                    a.insert(newPlace);
-                }
-                 */
-                } else {//the line we are on does not match the previous lines city state
-                    //here we should add the code to search a tree to make sure this city state
-                    //is new. if it is not new, then we simple add the zip to that place.
-                    Place newPlace2 = new Place(cityState1,zipCode1);//create a new place
+
+                } else {
+                    Place newPlace2 = new Place(cityState1, zipCode1);
                     s.insert(newPlace2);
                     b.insert(newPlace2);
                     a.insert(newPlace2);
-                    holdPlace = newPlace2;//sets our temp
-                /*
-                Place newPlace1 = new Place(cityState1, zipCode1);
-                if (data.hasNextLine()) {
-                    System.out.println("HERE");// Last line of file check
-                    s.insert(newPlace1);                 //delete the printstream
-                    b.insert(newPlace1);
-                    a.insert(newPlace1);
-                }
-
-                 */
-                    //lastName = cityState1;
+                    holdPlace = newPlace2;
                 }
             }
-            prevName = cityState1;//readjusts the prevname to what we just looked at so the next line will compare to this line
-           // System.out.println(prevName);
-
+            prevName = cityState1;
         }
     }
 
+    /**
+     * This method calculates the standard deviation and average # of searches for each tree
+     * @param data is the scanner thats called
+     * @param s    is the Splay Tree called
+     * @param b    is the BST tree called
+     * @param a    is the AVL tree called
+     */
+    public static void getResults(Scanner data, SplayTree<Place> s, BinarySearchTree<Place> b, AVLTree<Place> a) {
+        String firstLine = data.nextLine();//gets ignored
+        ArrayList<Integer> sHold = new ArrayList<Integer>();
+        ArrayList<Integer> bstHold = new ArrayList<Integer>();
+        ArrayList<Integer> avlHold = new ArrayList<Integer>();
+        while (data.hasNextLine()) {
+            //get the data for a city
+            int zipCode = data.nextInt();
+            double xValue = data.nextDouble(); //ignore
+            double yValue = data.nextDouble(); //ignore
+            String cityState = data.nextLine().trim();
+            Place searchFor = new Place(cityState, 0000);
+            SplayNode<Place> found = s.search(searchFor);
+            BSTNode<Place> found1 = b.search(searchFor);
+            AVLNode<Place> found2 = a.search(searchFor);
+            int splay = s.getSearchCounter();
+            int bst = b.getSearchCounter();
+            int avl = a.getSearchCounter();
+            sHold.add(splay);
+            bstHold.add(bst);
+            avlHold.add(avl);
+        }
+        int sumSplay = 0;
+        int sumBST = 0;
+        int sumAVL = 0;
+        double sum2 = 0;
+        double sdSplay = 0;
+        for (int i = 0; i < sHold.size(); i++) {
+            sumSplay += i;
+        }
+        for (int i = 0; i < bstHold.size(); i++) {
+            sumBST += i;
+        }
+        for (int i = 0; i < avlHold.size(); i++) {
+            sumAVL += i;
+        }
+        int avgSplay = sumSplay / sHold.size();
+        int avgBST = sumBST / bstHold.size();
+        int avgAVL = sumAVL / avlHold.size();
+        for (int i = 0; i < sHold.size(); i++) {
+            double fvalue = (Math.pow((sHold.get(i) - avgSplay), 2));
+            //x1_average[i] = fvalue;
+            System.out.println("test: " + fvalue);
+        }
+        sdSplay = Math.sqrt(sum2 / avlHold.size());
+        System.out.println("The average # of searches for the Splay tree was: " + avgSplay + " and the Standard Deviation is :");
+        System.out.println("The average # of searches for the BST tree was: " + avgBST);
+        System.out.println("The average # of searches for the AVL tree was: " + avgAVL);
+    }
 }
